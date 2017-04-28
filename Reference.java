89625b592a75a60fc7cee793e939dbc39db17484
Max W <wiedemax@rocketmail.com>
2017-04-28 22:58:13 +0200

    Delete Reference.java

--- Reference.java
+++
@@ -1,9 +0,0 @@
-package eyeronic.edt;
-
-public class Reference {
-	public static final String 
-			MOD_ID = "edt",
-			MOD_NAME = "Dynamic Torches",
-			MOD_VERSION = "1.0",
-			GUI_FACTORY = "eyeronic.edt.gui.DynamicTorchesGUIFactory";
-}
