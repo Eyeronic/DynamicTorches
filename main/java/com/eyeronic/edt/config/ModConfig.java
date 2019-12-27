6a538e1390a1200724f877dd0bd9bc99698e9b6b
Max W <wiedemax@rocketmail.com>
2019-12-27 11:57:24 +0100

    Update ModConfig.java
    removed //TODOs

--- main/java/com/eyeronic/edt/config/ModConfig.java
+++ main/java/com/eyeronic/edt/config/ModConfig.java
@@ -10,7 +10,6 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Config(modid = Reference.MODID)
 public class ModConfig {
-	//TODO: add functions that will allow torches to be moved to a valid position when set to false (i.e. NORTH, EAST, SOUTH, WEST, UP)
 	@Config.Name("Dynamic Torches active")
 	@Config.Comment("Only available while in main menu.\nWARNING! Deletes torches not placed in vanilla positions!")
 	@Config.RequiresMcRestart
@@ -21,12 +20,10 @@ public class ModConfig {
 	@Config.Comment("If enabled torches will switch to the ground when the wall they are attached to breaks and a valid block is directly under them.")
 	public static boolean shouldTorchesSwitchToGround = false;
 	
-	// TODO: clean up mode that actually updates blockstates and/or meta data saved --> DONE! Just takes a while since you can't run this on WorldEvent.Load.
 	@Config.Name("Clean Up mode active")
 	@Config.Comment("Enable before uninstalling EDT to save torches in vanilla positions where possible. Will take some time depending on number of torches placed in corners.")
 	public static boolean isCleanUp = false;
 	
-	//TODO: implement method to update torch positions on first run
 	@Config.Name("First Run mode active")
 	@Config.Comment("Enable to automatically move all loaded torches to corner positions where possible. Will take some time depending on number of torches loaded.")
 	public static boolean isFirstRun = true;
